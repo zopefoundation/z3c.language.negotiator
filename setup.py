1baab8c2ffa184c5ae7f2360546c83e06639088b
Adam Groszer <agroszer@mail.com>
2010-08-05 08:12:26 +0000

    Back to development: 1.1.3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.language.negotiator',
-    version='1.1.2',
+    version='1.1.3dev',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "Zope3 i18n language negotiator.",
